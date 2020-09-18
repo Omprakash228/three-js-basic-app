9a99af82e7da4903860f93fa7d0a65dadea513bb
Omprakash228 <omprakashnarayanan302@gmail.com>
2020-09-19 01:26:46 +0530

    Commit for responsive design and materials

--- js/app.js
+++ js/app.js
@@ -1,24 +1,61 @@
-const container = document.querySelector("#scene-container");
+let container;
+let camera;
+let renderer;
+let scene;
+let mesh;
 
-const scene = new THREE.Scene();
-scene.background = new THREE.Color("skyblue");
+function init() {
+  container = document.querySelector("#scene-container");
 
-const fov = 35;
-const aspect = container.clientWidth / container.clientHeight;
-const near = 0.1;
-const far = 100;
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.set(0, 0, 10);
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x8fbcd4);
 
-const geometry = new THREE.BoxBufferGeometry(2, 2, 2);
-const material = new THREE.MeshBasicMaterial();
-const mesh = new THREE.Mesh(geometry, material);
+  const fov = 35;
+  const aspect = container.clientWidth / container.clientHeight;
+  const near = 0.1;
+  const far = 100;
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0, 0, 10);
 
-scene.add(mesh);
+  const geometry = new THREE.BoxBufferGeometry(2, 2, 2);
+  const material = new THREE.MeshStandardMaterial({ color: 0x800080 });
+  mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(container.clientWidth, container.clientHeight);
-renderer.setPixelRatio(window.devicePixelRatio);
+  const light = new THREE.DirectionalLight(0xffffff, 5.0);
+  light.position.set(10, 10, 10);
+  scene.add(light);
 
-container.appendChild(renderer.domElement);
-renderer.render(scene, camera);
+  renderer = new THREE.WebGLRenderer({antialias: true});
+  renderer.setSize(container.clientWidth, container.clientHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  container.appendChild(renderer.domElement);
+
+  //VR and AR support or use window.requestAnimationFrame
+  renderer.setAnimationLoop(() => {
+      update();
+      render();
+  })
+}
+
+function update() {
+    mesh.rotation.z += 0.01;
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01;
+}
+
+function render() {
+    renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+    camera.aspect = container.clientWidth / container.clientHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(container.clientWidth, container.clientHeight);
+}
+
+window.addEventListener("resize", onWindowResize);
+
+init();
