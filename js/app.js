1d4a5f4b5ef2f28882590123be3bf4bbfc5001cb
Omprakash228 <omprakashnarayanan302@gmail.com>
2020-09-19 20:42:42 +0530

    Commit for orbit controls

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 let container;
 let camera;
+let controls;
 let renderer;
 let scene;
 let mesh;
@@ -10,58 +11,93 @@ function init() {
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0x8fbcd4);
 
+  createCamera();
+  createControls();
+  createLights();
+  createMeshes();
+  createRenderer(); 
+
+  //VR and AR support or use window.requestAnimationFrame
+  renderer.setAnimationLoop(() => {
+    update();
+    render();
+  });
+}
+
+function createCamera() {
   const fov = 35;
   const aspect = container.clientWidth / container.clientHeight;
   const near = 0.1;
   const far = 100;
   camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(0, 0, 10);
+  camera.position.set(-4, 4, 10);
+}
 
-  const geometry = new THREE.BoxBufferGeometry(2, 2, 2);
+function createControls() {
+  controls = new THREE.OrbitControls(camera, container);
+}
+
+function createLights() {
+  // const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
+  // scene.add(ambientLight);
+
+  // const mainLight = new THREE.DirectionalLight(0xffffff, 1);
+  // mainLight.position.set(10, 10, 10);
+
+  // scene.add(ambientLight, mainLight);
+
+  const ambientLight = new THREE.HemisphereLight(
+    0xddeeff,
+    0x202020,
+    5
+  );
+
+  const mainLight = new THREE.DirectionalLight(0xffffff, 5);
+  mainLight.position.set(10,10,10);
+
+  scene.add(ambientLight, mainLight);
+}
+
+function createMeshes() {
+  const geometry = new THREE.BoxBufferGeometry(2,2,2);
   const textureLoader = new THREE.TextureLoader();
   const texture = textureLoader.load('textures/uv_test_bw.png');
   texture.encoding = THREE.sRGBEncoding;
   texture.anisotropy = 16;
-  const material = new THREE.MeshStandardMaterial({ 
-      map: texture
-    });
+
+  const material = new THREE.MeshStandardMaterial({
+    map: texture
+  });
   mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
+}
 
-  const light = new THREE.DirectionalLight(0xffffff, 3);
-  light.position.set(10, 10, 10);
-  scene.add(light);
-
-  renderer = new THREE.WebGLRenderer({antialias: true});
+function createRenderer() {
+  renderer = new THREE.WebGLRenderer({
+    antialias: true
+  });
   renderer.setSize(container.clientWidth, container.clientHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.gammaFactor = 2.2;
   renderer.gammaOutput = true;
+  renderer.physicallyCorrectLights = true;
 
   container.appendChild(renderer.domElement);
-
-  //VR and AR support or use window.requestAnimationFrame
-  renderer.setAnimationLoop(() => {
-      update();
-      render();
-  })
 }
 
 function update() {
-    mesh.rotation.z += 0.01;
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.01;
+  
 }
 
 function render() {
-    renderer.render(scene, camera);
+  renderer.render(scene, camera);
 }
 
 function onWindowResize() {
-    camera.aspect = container.clientWidth / container.clientHeight;
-    camera.updateProjectionMatrix();
+  camera.aspect = container.clientWidth / container.clientHeight;
+  camera.updateProjectionMatrix();
 
-    renderer.setSize(container.clientWidth, container.clientHeight);
+  renderer.setSize(container.clientWidth, container.clientHeight);
 }
 
 window.addEventListener("resize", onWindowResize);
